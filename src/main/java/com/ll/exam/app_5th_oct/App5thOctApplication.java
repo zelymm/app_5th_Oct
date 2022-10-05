a365ef8ca05533c1526a73b574fa061d526c8187
zelymm <dkdlwkrz@naver.com>
2022-10-05 13:46:20 +0900

    initial commit

---
+++ src/main/java/com/ll/exam/app_5th_oct/App5thOctApplication.java
@@ -0,0 +1,13 @@
+package com.ll.exam.app_5th_oct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App5thOctApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App5thOctApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/exam/app_5th_oct/App5thOctApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.app_5th_oct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App5thOctApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
